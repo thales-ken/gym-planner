1da49409f56a23efa6ee33947e8816711aad9393
Thales Ken <thales_ken@hotmail.com>
2024-09-25 12:49:03 -0300

    refactor: remove unused props

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,7 @@
 import Logo from "./logo";
 import Navbar from "./navbar";
 
-interface HeaderProps {}
-
-export function Header(props: HeaderProps) {
+export function Header() {
     return (
         <header className='py-5 px-40'>
             <div className='flex flex-row items-center justify-between'>
